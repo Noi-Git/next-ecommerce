e02b1a1379c9192a108b3a4760ed2bdc9e42741e
Noi-Git <noi.patsin@gmail.com>
2024-12-05 19:38:06 -0500

    create item on the menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from 'next/image'
+import Link from 'next/link'
 import { useState } from 'react'
 
 const Menu = () => {
@@ -15,6 +16,16 @@ const Menu = () => {
         className='cursor-pointer'
         onClick={() => setOpen((prev) => !prev)}
       />
+      {open && (
+        <div className=''>
+          <Link href='/'>Homepage</Link>
+          <Link href='/'>Shop</Link>
+          <Link href='/'>Deals</Link>
+          <Link href='/'>Contact</Link>
+          <Link href='/'>Logout</Link>
+          <Link href='/'>Cart(1)</Link>
+        </div>
+      )}
     </div>
   )
 }
