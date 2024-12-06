713cef9c25f49ff26e7aed3e989f0ea03e3a98b4
Noi-Git <noi.patsin@gmail.com>
2024-12-05 19:26:29 -0500

    create menu page

---
+++ src/components/NavStyle.ts
@@ -0,0 +1,1 @@
+export const logo = 'h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:0x-64'
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,14 @@
+import Link from 'next/link'
+import { logo } from './NavStyle'
+import Menu from './Menu'
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className={logo}>
+      {/* MOBILE */}
+      <Link href={'/'}>NoiShop</Link>
+      <Menu />
+    </div>
+  )
 }
 
 export default Navbar
