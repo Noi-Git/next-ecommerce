a3704602c38a2cf689a6ed1a909f7247cb441b9b
Noi-Git <noi.patsin@gmail.com>
2024-12-05 20:52:24 -0500

    create seperate search and icon components and add hidden mobile menu on bigger screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,27 +9,32 @@ import {
 } from './NavStyle'
 import Menu from './Menu'
 import Image from 'next/image'
+import SearchBar from './SearchBar'
+import NavIcons from './NavIcons'
 const Navbar = () => {
   return (
     <div className={navMenuSize}>
       <div className={navMain}>
         {/* MOBILE */}
         <Link href={'/'}>
-          <div className='navLogo'>NOISHOP</div>
+          <div className={navLogo}>NOISHOP</div>
         </Link>
         <Menu />
       </div>
       {/* BIGGER SCREEN */}
-      <div className='navMenu'>
+      <div className={navMenu}>
         {/* LEFT */}
-        <div className='navMenuLeft'>
+        <div className={navMenuLeft}>
           <Link href='/'>
             <Image src='/logo.png' alt='' width={24} height={24} />
-            <div className='navLogo'>NOISHOP</div>
+            <div className={navLogo}>NOISHOP</div>
           </Link>
         </div>
         {/* RIGHT */}
-        <div className='navMenuRight'></div>
+        <div className={navMenuRight}>
+          <SearchBar />
+          <NavIcons />
+        </div>
       </div>
     </div>
   )
