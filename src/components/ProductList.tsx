55a5a9c81e4a4ba0519360e483596be84fc0dce9
Noi-Git <noi.patsin@gmail.com>
2024-12-21 15:50:28 -0500

    add style of responsive

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -4,9 +4,12 @@ import React from 'react'
 
 const ProductList = () => {
   return (
-    <div className='flex gap-x-8 gap-y-16 justify-between flex-wrap'>
+    <div className='mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap'>
       {/* When clicked -- will open a individual product page */}
-      <Link href='/test' className='w-full flex flex-col gap-4'>
+      <Link
+        href='/test'
+        className='w-full flex flex-col gap-4 sm:w-[45%] lg:[22%]'
+      >
         <div className='relative w-full h-80'>
           <Image
             src='https://images.pexels.com/photos/372166/pexels-photo-372166.jpeg?auto=compress&cs=tinysrgb&w=800'
