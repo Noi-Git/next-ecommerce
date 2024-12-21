734a105a522678c07f84d624fff3a2d4b6b633b1
Noi-Git <noi.patsin@gmail.com>
2024-12-21 16:52:36 -0500

    create category list item and style

---
+++ src/components/CategoryList.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import {
+  categoryListContainer,
+  categoryListImage,
+  categoryListImageWrapper,
+  categoryListLink,
+  categoryListLinkWrapper,
+  categoryName,
+} from './Styles/CategoryList'
+
+const CategoryList = () => {
+  return (
+    <div className={categoryListContainer}>
+      <div className={categoryListLinkWrapper}>
+        <Link href='/list?cat=test' className={categoryListLink}>
+          <div className={categoryListImageWrapper}>
+            <Image
+              src='https://images.pexels.com/photos/33393/caudata-strelitzia-bird-of-paradise-flower-strelitzia-orchids.jpg?auto=compress&cs=tinysrgb&w=800'
+              alt=''
+              fill
+              sizes='20vw'
+              className={categoryListImage}
+            />
+          </div>
+          <h1 className={categoryName}>Category Name</h1>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default CategoryList
