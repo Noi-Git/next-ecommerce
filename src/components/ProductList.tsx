abd0f8e55b36f3b187fb10964b337db2fa8e716f
Noi-Git <noi.patsin@gmail.com>
2024-12-21 16:11:17 -0500

    add more images

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -16,7 +16,7 @@ const ProductList = () => {
       <Link href='/test' className={featuredProductsItem}>
         <div className={featuredProductsImageWrapper}>
           <Image
-            src='https://images.pexels.com/photos/372166/pexels-photo-372166.jpeg?auto=compress&cs=tinysrgb&w=800'
+            src='https://images.pexels.com/photos/264787/pexels-photo-264787.jpeg?auto=compress&cs=tinysrgb&w=800'
             alt=''
             fill
             sizes='25vw'
@@ -24,7 +24,89 @@ const ProductList = () => {
           />
           {/* Add second image of hover-over */}
           <Image
-            src='https://images.pexels.com/photos/945453/pexels-photo-945453.jpeg?auto=compress&cs=tinysrgb&w=800'
+            src='https://images.pexels.com/photos/1073567/pexels-photo-1073567.jpeg?auto=compress&cs=tinysrgb&w=800'
+            alt=''
+            fill
+            sizes='25vw'
+            className={featuredProductsSecondImage}
+          />
+        </div>
+        <div className='flex justify-between'>
+          <span className='font-medium'>Product Name</span>
+          <span className='font-semibold'>$49</span>
+        </div>
+        <div className='text-sm text-gray-500'>My description</div>
+        <button className='rounded-2xl ring-1 ring-salmon-color w-max text-salmon-color py-2 px-4 text-xs hover:bg-salmon-color hover:text-white'>
+          Add to Cart
+        </button>
+      </Link>
+      {/* ===== */}
+      <Link href='/test' className={featuredProductsItem}>
+        <div className={featuredProductsImageWrapper}>
+          <Image
+            src='https://images.pexels.com/photos/33393/caudata-strelitzia-bird-of-paradise-flower-strelitzia-orchids.jpg?auto=compress&cs=tinysrgb&w=800'
+            alt=''
+            fill
+            sizes='25vw'
+            className={featuredProductsFirstImage}
+          />
+          {/* Add second image of hover-over */}
+          <Image
+            src='https://images.pexels.com/photos/674475/pexels-photo-674475.jpeg?auto=compress&cs=tinysrgb&w=800'
+            alt=''
+            fill
+            sizes='25vw'
+            className={featuredProductsSecondImage}
+          />
+        </div>
+        <div className='flex justify-between'>
+          <span className='font-medium'>Product Name</span>
+          <span className='font-semibold'>$49</span>
+        </div>
+        <div className='text-sm text-gray-500'>My description</div>
+        <button className='rounded-2xl ring-1 ring-salmon-color w-max text-salmon-color py-2 px-4 text-xs hover:bg-salmon-color hover:text-white'>
+          Add to Cart
+        </button>
+      </Link>
+      <Link href='/test' className={featuredProductsItem}>
+        <div className={featuredProductsImageWrapper}>
+          <Image
+            src='https://images.pexels.com/photos/697256/pexels-photo-697256.jpeg?auto=compress&cs=tinysrgb&w=800'
+            alt=''
+            fill
+            sizes='25vw'
+            className={featuredProductsFirstImage}
+          />
+          {/* Add second image of hover-over */}
+          <Image
+            src='https://images.pexels.com/photos/775779/pexels-photo-775779.jpeg?auto=compress&cs=tinysrgb&w=800'
+            alt=''
+            fill
+            sizes='25vw'
+            className={featuredProductsSecondImage}
+          />
+        </div>
+        <div className='flex justify-between'>
+          <span className='font-medium'>Product Name</span>
+          <span className='font-semibold'>$49</span>
+        </div>
+        <div className='text-sm text-gray-500'>My description</div>
+        <button className='rounded-2xl ring-1 ring-salmon-color w-max text-salmon-color py-2 px-4 text-xs hover:bg-salmon-color hover:text-white'>
+          Add to Cart
+        </button>
+      </Link>
+      <Link href='/test' className={featuredProductsItem}>
+        <div className={featuredProductsImageWrapper}>
+          <Image
+            src='https://images.pexels.com/photos/54319/cranesbill-blossom-bloom-blue-54319.jpeg?auto=compress&cs=tinysrgb&w=800'
+            alt=''
+            fill
+            sizes='25vw'
+            className={featuredProductsFirstImage}
+          />
+          {/* Add second image of hover-over */}
+          <Image
+            src='https://images.pexels.com/photos/56856/hibiscus-blossom-bloom-flower-56856.jpeg?auto=compress&cs=tinysrgb&w=800'
             alt=''
             fill
             sizes='25vw'
