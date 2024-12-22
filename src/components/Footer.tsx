da5f31628c0edb3dcead532a4b0eb9e14a45fe7b
Noi-Git <noi.patsin@gmail.com>
2024-12-22 10:43:00 -0500

    create center session, it will be hidden on small and medium screen

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,7 +25,41 @@ const Footer = () => {
           </div>
         </div>
         {/* CENTER */}
-        <div className='hidden lg:flex justify-between w-1/2'></div>
+        <div className='hidden lg:flex justify-between w-1/2'>
+          {/* ==== COMPANY ==== */}
+          <div className='flex flex-col justify-between'>
+            <h1 className='font-medium text-lg'>COMPANY</h1>
+            <div className='flex flex-col gap-6'>
+              <Link href=''>About Us</Link>
+              <Link href=''>Careers</Link>
+              <Link href=''>Affiliates</Link>
+              <Link href=''>Blog</Link>
+              <Link href=''>Contact Us</Link>
+            </div>
+          </div>
+          {/* ==== SHOP ==== */}
+          <div className='flex flex-col justify-between'>
+            <h1 className='font-medium text-lg'>COMPANY</h1>
+            <div className='flex flex-col gap-6'>
+              <Link href=''>New Arrivals</Link>
+              <Link href=''>Accessories</Link>
+              <Link href=''>Men</Link>
+              <Link href=''>Women</Link>
+              <Link href=''>All Products</Link>
+            </div>
+          </div>
+          {/* ==== CUSTOMER SERVICE */}
+          <div className='flex flex-col justify-between'>
+            <h1 className='font-medium text-lg'>COMPANY</h1>
+            <div className='flex flex-col gap-6'>
+              <Link href=''>Customer Service</Link>
+              <Link href=''>My Account</Link>
+              <Link href=''>Find a Store</Link>
+              <Link href=''>Legal & Privacy</Link>
+              <Link href=''>Gift Card</Link>
+            </div>
+          </div>
+        </div>
         {/* RIGHT */}
         <div className='w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8'>
           <h1 className='font-medium text-lg'>SUBSCRIBE</h1>
