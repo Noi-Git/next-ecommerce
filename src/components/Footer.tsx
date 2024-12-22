4e0606e4977dd61dd0d9c6b5d30a9a878a56547f
Noi-Git <noi.patsin@gmail.com>
2024-12-21 20:47:43 -0500

    footer structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,19 @@
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div>
+      {/* TOP */}
+      <div className='top'>
+        {/* LEFT */}
+        <div className='w-1/4'></div>
+        {/* CENTER */}
+        <div className='w-1/2'></div>
+        {/* RIGHT */}
+        <div className='w-1/4'></div>
+      </div>
+      {/* BOTTOM */}
+      <div className='bottom'></div>
+    </div>
+  )
 }
 
 export default Footer
