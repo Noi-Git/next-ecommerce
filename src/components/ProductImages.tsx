178d8c95b2f2ec3ffb766c51df137c185cbf5ec8
Noi-Git <noi.patsin@gmail.com>
2024-12-25 16:56:41 -0500

    create structure to display large and small images on the single page

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -1,7 +1,33 @@
+import Image from 'next/image'
 import React from 'react'
 
 const ProductImages = () => {
-  return <div>ProductImages</div>
+  return (
+    <div>
+      {/* BIG IMAGE */}
+      <div className='h-[500px] replattive'>
+        <Image
+          src='https://images.pexels.com/photos/54319/cranesbill-blossom-bloom-blue-54319.jpeg?auto=compress&cs=tinysrgb&w=800'
+          alt=''
+          fill
+          sizes='50vw'
+          className='object-cover rounded-md'
+        />
+      </div>
+      {/* SMALL IMAGE */}
+      <div className=''>
+        <div className='w-1/4 h-32 replattive gap-4 mt-8'>
+          <Image
+            src='https://images.pexels.com/photos/54319/cranesbill-blossom-bloom-blue-54319.jpeg?auto=compress&cs=tinysrgb&w=800'
+            alt=''
+            fill
+            sizes='30vw'
+            className='object-cover rounded-md'
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ProductImages
