23da12ce355046a72b3757901d1e665f5abe7869
Noi-Git <noi.patsin@gmail.com>
2024-12-25 15:55:58 -0500

    setup single page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,7 +1,17 @@
+import ProductImages from '@/components/ProductImages'
 import React from 'react'
 
 const SinglePage = () => {
-  return <div> SinglePage</div>
+  return (
+    <div>
+      {/* IMAGE */}
+      <div className=''>
+        <ProductImages />
+      </div>
+      {/* TEXT */}
+      <div className=''></div>
+    </div>
+  )
 }
 
 export default SinglePage
