ca4887eae6ac62527da93f413402c439f29f1fa2
Noi-Git <noi.patsin@gmail.com>
2024-12-25 16:00:34 -0500

    setup single page structure

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 const SinglePage = () => {
   return (
-    <div>
+    <div className='md:px-8 lg:px-16 xl:32 2xl:px-64 relative flex flex-col lg:flex-row gap-16'>
       {/* IMAGE */}
-      <div className=''>
+      <div className='w-full lg:w-1/2 lg:sticky top-20 h-max'>
         <ProductImages />
       </div>
       {/* TEXT */}
-      <div className=''></div>
+      <div className='w-full lg:w-1/2 flex flex-col gap-6'>Texts</div>
     </div>
   )
 }
