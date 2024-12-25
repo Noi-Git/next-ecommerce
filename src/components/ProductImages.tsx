72a328f590c75bcd3f2e68c5702cd6d710d0aca4
Noi-Git <noi.patsin@gmail.com>
2024-12-25 17:04:31 -0500

    setup images object and use for display large image

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -1,13 +1,34 @@
-import Image from 'next/image'
-import React from 'react'
+'use client'
 
+import Image from 'next/image'
+import React, { useState } from 'react'
+
+const images = [
+  {
+    id: 1,
+    url: 'https://images.pexels.com/photos/264787/pexels-photo-264787.jpeg?auto=compress&cs=tinysrgb&w=800',
+  },
+  {
+    id: 2,
+    url: 'https://images.pexels.com/photos/1073567/pexels-photo-1073567.jpeg?auto=compress&cs=tinysrgb&w=800',
+  },
+  {
+    id: 3,
+    url: 'https://images.pexels.com/photos/33393/caudata-strelitzia-bird-of-paradise-flower-strelitzia-orchids.jpg?auto=compress&cs=tinysrgb&w=800',
+  },
+  {
+    id: 4,
+    url: 'https://images.pexels.com/photos/674475/pexels-photo-674475.jpeg?auto=compress&cs=tinysrgb&w=800',
+  },
+]
 const ProductImages = () => {
+  const [index, setIndex] = useState(0)
   return (
     <div>
       {/* BIG IMAGE */}
       <div className='h-[500px] replattive'>
         <Image
-          src='https://images.pexels.com/photos/54319/cranesbill-blossom-bloom-blue-54319.jpeg?auto=compress&cs=tinysrgb&w=800'
+          src={images[index].url}
           alt=''
           fill
           sizes='50vw'
