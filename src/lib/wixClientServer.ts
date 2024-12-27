2183324a1f8a874f4481d9a571a350c51f1687ec
Noi-Git <noi.patsin@gmail.com>
2024-12-27 15:27:59 -0500

    wrap cookies in the try..catch and export the whole function

--- src/lib/wixClientServer.ts
+++ src/lib/wixClientServer.ts
@@ -2,23 +2,31 @@ import { createClient, OAuthStrategy } from '@wix/sdk'
 import { products, collections } from '@wix/stores'
 import { cookies } from 'next/headers'
 
-const cookieStore = cookies()
-
-const refreshToken = JSON.parse(cookieStore.get('refreshToken')?.value || '{}')
-// USE THIS FUNCTION TO PATCH DATA
-export const wixClientServer = createClient({
-  modules: {
-    products,
-    collections,
-  },
-  auth: OAuthStrategy({
-    clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
-    tokens: {
-      refreshToken,
-      accessToken: {
-        value: '',
-        expiresAt: 0,
-      },
+// CREATE A FUNCTION TO RETURN wixClientServer
+export const wixClientServer = async () => {
+  let refreshToken
+  // USE next/headers cookies TO READ COOKIE
+  try {
+    //CREATE COOKIES
+    const cookieStore = cookies()
+    refreshToken = JSON.parse(cookieStore.get('refreshToken')?.value || '{}')
+  } catch (error) {}
+  // USE THIS FUNCTION TO PATCH DATA
+  const wixClient = createClient({
+    modules: {
+      products,
+      collections,
     },
-  }),
-})
+    auth: OAuthStrategy({
+      clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
+      tokens: {
+        refreshToken,
+        accessToken: {
+          value: '',
+          expiresAt: 0,
+        },
+      },
+    }),
+  })
+  return wixClient
+}
