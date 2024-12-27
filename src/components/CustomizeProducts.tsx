9bca77843d17d119962a2054441f6690c95c44a5
Noi-Git <noi.patsin@gmail.com>
2024-12-26 19:53:22 -0500

    structure customize product section

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const CustomizeProducts = () => {
-  return <div>CustomizeProducts</div>
+  return (
+    <div className='flex flex-col gap-6'>
+      {/* CHOOSE COLOR */}
+      <h4 className='font-medium'>Choose a color</h4>
+      <div className='flex items-center gap-3'></div>
+      {/* CHOOSE SIZE */}
+      <h4 className='font-medium'>Choose a size</h4>
+      <div className='flex items-center gap-3'></div>
+    </div>
+  )
 }
 
 export default CustomizeProducts
