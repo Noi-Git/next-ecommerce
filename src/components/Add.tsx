182db481dd7d358ef18d074e4ec797cfcd5002a8
Noi-Git <noi.patsin@gmail.com>
2024-12-26 20:36:35 -0500

    create and and remove quantity button using useState

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,15 +1,20 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { addToCartButton } from './Styles/AddStyle'
 
 const Add = () => {
+  const [quantity, setQuantity] = useState(1)
   return (
     <div className='flex flex-col gap-4'>
       <h4 className='font-medium'>Choose a Quantity</h4>
       <div className=''>
         {/* SELECT QUANTITY BUTTON */}
-        <div className=''></div>
+        <div className=''>
+          <button className=''>-</button>
+          {quantity}
+          <button className=''>+</button>
+        </div>
         {/* SELECT QUANTITY TEXT */}
         <div className='text-xs'>
           Only <span className='text-orange-500'>4 items</span> left! <br />
