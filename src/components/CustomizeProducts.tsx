90f75e0bf1dec17e4867ec5f00f0f83b051eea61
Noi-Git <noi.patsin@gmail.com>
2024-12-26 20:19:07 -0500

    create product size selection buttons

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -5,22 +5,31 @@ const CustomizeProducts = () => {
     <div className='flex flex-col gap-6'>
       {/* CHOOSE COLOR */}
       <h4 className='font-medium'>Choose a color</h4>
-      <div className='flex items-center gap-3'>
-        <ul className='flex items-center gap-3'>
-          {/* INSIDE CIRCLE */}
-          <li className='w-8 h-8 rounded-full right-1 ring-gray-300 cursor-pointer relative bg-red-500 '>
-            {/* OUTSIDE CIRCLE - WHEN ITEM SELECTED */}
-            <div className='absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'></div>
-          </li>
-          <li className='w-8 h-8 rounded-full right-1 ring-gray-300 cursor-pointer relative bg-blue-500 '></li>
-          <li className='w-8 h-8 rounded-full right-1 ring-gray-300 cursor-not-allowed relative bg-green-500 '>
-            <div className='absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'></div>
-          </li>
-        </ul>
-      </div>
+      <ul className='flex items-center gap-3'>
+        {/* INSIDE CIRCLE */}
+        <li className='w-8 h-8 rounded-full right-1 ring-gray-300 cursor-pointer relative bg-red-500 '>
+          {/* OUTSIDE CIRCLE - WHEN ITEM SELECTED */}
+          <div className='absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'></div>
+        </li>
+        <li className='w-8 h-8 rounded-full right-1 ring-gray-300 cursor-pointer relative bg-blue-500 '></li>
+        <li className='w-8 h-8 rounded-full right-1 ring-gray-300 cursor-not-allowed relative bg-green-500 '>
+          <div className='absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'></div>
+        </li>
+      </ul>
+
       {/* CHOOSE SIZE */}
       <h4 className='font-medium'>Choose a size</h4>
-      <div className='flex items-center gap-3'></div>
+      <ul className='flex items-center gap-3'>
+        <li className='ring-1 ring-salmon-color text-salmon-color rounded-md py-1 px-4 text-sm cursor-pointer'>
+          Small
+        </li>
+        <li className='ring-1 ring-salmon-color text-white bg-salmon-color rounded-md py-1 px-4 text-sm cursor-pointer'>
+          Medium
+        </li>
+        <li className='ring-1 ring-pink-200 text-white bg-pink-200 rounded-md py-1 px-4 text-sm cursor-not-allowed'>
+          Large
+        </li>
+      </ul>
     </div>
   )
 }
