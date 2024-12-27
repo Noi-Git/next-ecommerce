5a3ea39fc43f6c98d2f49b484f7e8a52488c9df1
Noi-Git <noi.patsin@gmail.com>
2024-12-26 20:41:17 -0500

    add style to add and remove button

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -8,20 +8,22 @@ const Add = () => {
   return (
     <div className='flex flex-col gap-4'>
       <h4 className='font-medium'>Choose a Quantity</h4>
-      <div className=''>
-        {/* SELECT QUANTITY BUTTON */}
-        <div className=''>
-          <button className=''>-</button>
-          {quantity}
-          <button className=''>+</button>
+      <div className='flex justify-between'>
+        <div className='flex items-center gap-4'>
+          {/* SELECT QUANTITY BUTTON */}
+          <div className='bg-gray-100 py-2 px-4 rounded-3xl flex items-center justify-between w-32'>
+            <button className='cursor-pointer text-xl'>-</button>
+            {quantity}
+            <button className='cursor-pointer text-xl'>+</button>
+          </div>
+          {/* SELECT QUANTITY TEXT */}
+          <div className='text-xs'>
+            Only <span className='text-orange-500'>4 items</span> left! <br />
+            {"Don't"} miss it
+          </div>
         </div>
-        {/* SELECT QUANTITY TEXT */}
-        <div className='text-xs'>
-          Only <span className='text-orange-500'>4 items</span> left! <br />
-          {"Don't"} miss it
-        </div>
+        <button className={addToCartButton}>Add to Cart</button>
       </div>
-      <button className={addToCartButton}>Add to Cart</button>
     </div>
   )
 }
