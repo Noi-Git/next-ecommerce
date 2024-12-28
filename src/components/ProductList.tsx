1a83634c7217e0ff8bc69b68be7ff6fcc7506fde
Noi-Git <noi.patsin@gmail.com>
2024-12-27 20:09:00 -0500

    loop through data from api

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -10,6 +10,7 @@ import {
   featuredProductsSecondImage,
 } from './Styles/ProductListStyle'
 import { wixClientServer } from '@/lib/wixClientServer'
+import { products } from '@wix/stores'
 
 const PRODUCT_PER_PAGE = 20
 
@@ -35,108 +36,33 @@ const ProductList = async ({
     .find()
   return (
     <div className={featuredProductsContainer}>
-      {/* When clicked -- will open a individual product page */}
-      <Link href='/test' className={featuredProductsItem}>
-        <div className={featuredProductsImageWrapper}>
-          <Image
-            src='https://images.pexels.com/photos/264787/pexels-photo-264787.jpeg?auto=compress&cs=tinysrgb&w=800'
-            alt=''
-            fill
-            sizes='25vw'
-            className={featuredProductsFirstImage}
-          />
-          {/* Add second image of hover-over */}
-          <Image
-            src='https://images.pexels.com/photos/1073567/pexels-photo-1073567.jpeg?auto=compress&cs=tinysrgb&w=800'
-            alt=''
-            fill
-            sizes='25vw'
-            className={featuredProductsSecondImage}
-          />
-        </div>
-        <div className='flex justify-between'>
-          <span className='font-medium'>Product Name</span>
-          <span className='font-semibold'>$49</span>
-        </div>
-        <div className='text-sm text-gray-500'>My description</div>
-        <button className={featuredProductsButton}>Add to Cart</button>
-      </Link>
-      {/* ===== */}
-      <Link href='/test' className={featuredProductsItem}>
-        <div className={featuredProductsImageWrapper}>
-          <Image
-            src='https://images.pexels.com/photos/33393/caudata-strelitzia-bird-of-paradise-flower-strelitzia-orchids.jpg?auto=compress&cs=tinysrgb&w=800'
-            alt=''
-            fill
-            sizes='25vw'
-            className={featuredProductsFirstImage}
-          />
-          {/* Add second image of hover-over */}
-          <Image
-            src='https://images.pexels.com/photos/674475/pexels-photo-674475.jpeg?auto=compress&cs=tinysrgb&w=800'
-            alt=''
-            fill
-            sizes='25vw'
-            className={featuredProductsSecondImage}
-          />
-        </div>
-        <div className='flex justify-between'>
-          <span className='font-medium'>Product Name</span>
-          <span className='font-semibold'>$49</span>
-        </div>
-        <div className='text-sm text-gray-500'>My description</div>
-        <button className={featuredProductsButton}>Add to Cart</button>
-      </Link>
-      <Link href='/test' className={featuredProductsItem}>
-        <div className={featuredProductsImageWrapper}>
-          <Image
-            src='https://images.pexels.com/photos/697256/pexels-photo-697256.jpeg?auto=compress&cs=tinysrgb&w=800'
-            alt=''
-            fill
-            sizes='25vw'
-            className={featuredProductsFirstImage}
-          />
-          {/* Add second image of hover-over */}
-          <Image
-            src='https://images.pexels.com/photos/775779/pexels-photo-775779.jpeg?auto=compress&cs=tinysrgb&w=800'
-            alt=''
-            fill
-            sizes='25vw'
-            className={featuredProductsSecondImage}
-          />
-        </div>
-        <div className='flex justify-between'>
-          <span className='font-medium'>Product Name</span>
-          <span className='font-semibold'>$49</span>
-        </div>
-        <div className='text-sm text-gray-500'>My description</div>
-        <button className={featuredProductsButton}>Add to Cart</button>
-      </Link>
-      <Link href='/test' className={featuredProductsItem}>
-        <div className={featuredProductsImageWrapper}>
-          <Image
-            src='https://images.pexels.com/photos/54319/cranesbill-blossom-bloom-blue-54319.jpeg?auto=compress&cs=tinysrgb&w=800'
-            alt=''
-            fill
-            sizes='25vw'
-            className={featuredProductsFirstImage}
-          />
-          {/* Add second image of hover-over */}
-          <Image
-            src='https://images.pexels.com/photos/56856/hibiscus-blossom-bloom-flower-56856.jpeg?auto=compress&cs=tinysrgb&w=800'
-            alt=''
-            fill
-            sizes='25vw'
-            className={featuredProductsSecondImage}
-          />
-        </div>
-        <div className='flex justify-between'>
-          <span className='font-medium'>Product Name</span>
-          <span className='font-semibold'>$49</span>
-        </div>
-        <div className='text-sm text-gray-500'>My description</div>
-        <button className={featuredProductsButton}>Add to Cart</button>
-      </Link>
+      {res.items.map((product: products.Product) => (
+        <Link href='/test' key={product._id} className={featuredProductsItem}>
+          <div className={featuredProductsImageWrapper}>
+            <Image
+              src='https://images.pexels.com/photos/264787/pexels-photo-264787.jpeg?auto=compress&cs=tinysrgb&w=800'
+              alt=''
+              fill
+              sizes='25vw'
+              className={featuredProductsFirstImage}
+            />
+            {/* Add second image of hover-over */}
+            <Image
+              src='https://images.pexels.com/photos/1073567/pexels-photo-1073567.jpeg?auto=compress&cs=tinysrgb&w=800'
+              alt=''
+              fill
+              sizes='25vw'
+              className={featuredProductsSecondImage}
+            />
+          </div>
+          <div className='flex justify-between'>
+            <span className='font-medium'>Product Name</span>
+            <span className='font-semibold'>$49</span>
+          </div>
+          <div className='text-sm text-gray-500'>My description</div>
+          <button className={featuredProductsButton}>Add to Cart</button>
+        </Link>
+      ))}
     </div>
   )
 }
