444590d0a6a02b3043aeeb3f8f36a02fcbcda50c
Noi-Git <noi.patsin@gmail.com>
2024-12-27 21:17:50 -0500

    loop throgh categories data from api

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -13,84 +13,29 @@ import { wixClientServer } from '@/lib/wixClientServer'
 
 const CategoryList = async () => {
   const wixClient = await wixClientServer()
-  const cat = wixClient.collections.queryCollections().find()
+  const cats = await wixClient.collections.queryCollections().find()
 
   return (
     <div className={categoryListContainer}>
       <div className={categoryListLinkWrapper}>
-        <Link href='/list?cat=test' className={categoryListLink}>
-          <div className={categoryListImageWrapper}>
-            <Image
-              src='https://images.pexels.com/photos/60702/blossom-bloom-flower-wild-flower-60702.jpeg?auto=compress&cs=tinysrgb&w=800'
-              alt=''
-              fill
-              sizes='20vw'
-              className={categoryListImage}
-            />
-          </div>
-          <h1 className={categoryName}>Category Name</h1>
-        </Link>
-        {/* ==== */}
-        <Link href='/list?cat=test' className={categoryListLink}>
-          <div className={categoryListImageWrapper}>
-            <Image
-              src='https://images.pexels.com/photos/60702/blossom-bloom-flower-wild-flower-60702.jpeg?auto=compress&cs=tinysrgb&w=800'
-              alt=''
-              fill
-              sizes='20vw'
-              className={categoryListImage}
-            />
-          </div>
-          <h1 className={categoryName}>Category Name</h1>
-        </Link>
-        <Link href='/list?cat=test' className={categoryListLink}>
-          <div className={categoryListImageWrapper}>
-            <Image
-              src='https://images.pexels.com/photos/60702/blossom-bloom-flower-wild-flower-60702.jpeg?auto=compress&cs=tinysrgb&w=800'
-              alt=''
-              fill
-              sizes='20vw'
-              className={categoryListImage}
-            />
-          </div>
-          <h1 className={categoryName}>Category Name</h1>
-        </Link>
-        <Link href='/list?cat=test' className={categoryListLink}>
-          <div className={categoryListImageWrapper}>
-            <Image
-              src='https://images.pexels.com/photos/60702/blossom-bloom-flower-wild-flower-60702.jpeg?auto=compress&cs=tinysrgb&w=800'
-              alt=''
-              fill
-              sizes='20vw'
-              className={categoryListImage}
-            />
-          </div>
-          <h1 className={categoryName}>Category Name</h1>
-        </Link>
-        <Link href='/list?cat=test' className={categoryListLink}>
-          <div className={categoryListImageWrapper}>
-            <Image
-              src='https://images.pexels.com/photos/60702/blossom-bloom-flower-wild-flower-60702.jpeg?auto=compress&cs=tinysrgb&w=800'
-              alt=''
-              fill
-              sizes='20vw'
-              className={categoryListImage}
-            />
-          </div>
-          <h1 className={categoryName}>Category Name</h1>
-        </Link>
-        <Link href='/list?cat=test' className={categoryListLink}>
-          <div className={categoryListImageWrapper}>
-            <Image
-              src='https://images.pexels.com/photos/60702/blossom-bloom-flower-wild-flower-60702.jpeg?auto=compress&cs=tinysrgb&w=800'
-              alt=''
-              fill
-              sizes='20vw'
-              className={categoryListImage}
-            />
-          </div>
-          <h1 className={categoryName}>Category Name</h1>
-        </Link>
+        {cats.items.map((item) => (
+          <Link
+            href='/list?cat=test'
+            className={categoryListLink}
+            key={item.id}
+          >
+            <div className={categoryListImageWrapper}>
+              <Image
+                src='https://images.pexels.com/photos/60702/blossom-bloom-flower-wild-flower-60702.jpeg?auto=compress&cs=tinysrgb&w=800'
+                alt=''
+                fill
+                sizes='20vw'
+                className={categoryListImage}
+              />
+            </div>
+            <h1 className={categoryName}>Category Name</h1>
+          </Link>
+        ))}
       </div>
     </div>
   )
