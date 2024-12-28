fe1c8a0e9491e70cb68160da75df89ea237a5d71
Noi-Git <noi.patsin@gmail.com>
2024-12-27 20:14:14 -0500

    fix catgoryId error by adding !

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ const HomePage = async () => {
         <h1 className='text-2xl'>Featured Products</h1>
         <Suspense fallback={'loading'}>
           <ProductList
-            categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID}
+            categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
             limit={4}
           />
         </Suspense>
@@ -47,7 +47,10 @@ const HomePage = async () => {
       </div>
       <div className='mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64'>
         <h1 className='text-2xl'>New Products</h1>
-        <ProductList />
+        <ProductList
+          categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
+          limit={4}
+        />
       </div>
     </div>
   )
