a139f0e94ee4008271ea98f86cf2eabb8e87e896
Noi-Git <noi.patsin@gmail.com>
2024-12-29 21:10:37 -0500

    display data from server and pass image in as props

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import Add from '@/components/Add'
 import CustomizeProducts from '@/components/CustomizeProducts'
 import ProductImages from '@/components/ProductImages'
 import { wixClientServer } from '@/lib/wixClientServer'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
 const SinglePage = async ({ params }: { params: { slug: string } }) => {
@@ -9,30 +10,37 @@ const SinglePage = async ({ params }: { params: { slug: string } }) => {
 
   //Fetch single page data
   const wixClient = await wixClientServer()
-  const res = await wixClient.products
+  const products = await wixClient.products
     .queryProducts()
     .eq('slug', params.slug)
     .find()
+  // console.log('🚀 ~ SinglePage ~ products:', products)
+
+  if (!products.items[0]) {
+    return notFound()
+  }
+
+  const product = products.items[0]
+  // console.log('🚀 ~ SinglePage ~ product.price:', product.price)
 
   return (
     <div className='px-5 md:px-8 lg:px-16 xl:32 2xl:px-64 relative flex flex-col lg:flex-row gap-16'>
       {/* IMAGE */}
       <div className='w-full lg:w-1/2 lg:sticky top-20 h-max'>
-        <ProductImages />
+        <ProductImages items={product.media?.items} />
       </div>
       {/* TEXT */}
       <div className='w-full lg:w-1/2 flex flex-col gap-6'>
-        <h1 className='text-4xl font-medium'>Product Name</h1>
-        <p className='text-gray-500'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia iure
-          ducimus magnam explicabo, culpa distinctio. Officiis eius, nobis
-          reiciendis ex ut excepturi distinctio quo, ratione consectetur
-          aliquam, iure architecto perferendis aperiam ab.
-        </p>
+        <h1 className='text-4xl font-medium'>{product.name}</h1>
+        <p className='text-gray-500'>{product.description}</p>
         <div className='h-[2px] bg-gray-100' />
         <div className='flex items-center gap-4'>
-          <h3 className='text-xl text-gray-500 line-through'>$59</h3>
-          <h2 className='font-medium text-2xl'>$49</h2>
+          <h3 className='text-xl text-gray-500 line-through'>
+            {product.price?.price}
+          </h3>
+          <h2 className='font-medium text-2xl'>
+            {product.price?.discountedPrice}
+          </h2>
         </div>
         <div className='h-[2px] bg-gray-100' />
         <CustomizeProducts />
