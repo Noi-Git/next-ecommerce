c9a77e1755cf3939dbb599ffe2574d33fc098dfc
Noi-Git <noi.patsin@gmail.com>
2025-01-11 16:02:23 -0500

    add on change function by using next navigation hook

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,12 +1,31 @@
+'use client'
 import React from 'react'
 import { filterInput, filterSelect, filterSortBy } from './Styles/FilterStyle'
+import { useParams, useSearchParams, useRouter } from 'next/navigation'
 
 const Filter = () => {
+  const pathname = useParams()
+  const searchPharams = useSearchParams()
+  const { replace } = useRouter()
+
+  const handleFilterChange = (
+    e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>
+  ) => {
+    const { name, value } = e.target
+    console.log('🚀 ~ handleFilterChange ~ value:', value)
+    console.log('🚀 ~ handleFilterChange ~ name:', name)
+  }
+
   return (
     <div className='mt-12 flex justify-between'>
       {/* LEFT SIDE */}
       <div className='flex gap-6 flex-wrap'>
-        <select name='type' id='' className={filterSelect}>
+        <select
+          name='type'
+          id=''
+          className={filterSelect}
+          onChange={handleFilterChange}
+        >
           <option>Type</option>
           <option value='physical'>Physical</option>
           <option value='digital'>Digital</option>
@@ -16,29 +35,32 @@ const Filter = () => {
           name='min'
           placeholder='min price'
           className={filterInput}
+          onChange={handleFilterChange}
         />
         <input
           type='text'
           name='max'
           placeholder='max price'
           className={filterInput}
+          onChange={handleFilterChange}
         />
-        <select name='size' id='' className={filterSelect}>
-          <option>Size</option>
-          <option value=''>Size</option>
-        </select>
-        <select name='color' id='' className={filterSelect}>
-          <option>Color</option>
-          <option value='test'>Test</option>
-        </select>
-        <select name='category' id='' className={filterSelect}>
+
+        <select name='cat' id='' className={filterSelect}>
           <option>Category</option>
           <option value=''>New Arrival</option>
           <option value=''>Popular</option>
         </select>
+        <select name='' id='' className={filterSelect}>
+          <option>All Filters</option>
+        </select>
       </div>
       {/* RIGHT SIDE */}
-      <select name='' id='' className={filterSortBy}>
+      <select
+        name='sort'
+        id=''
+        className={filterSortBy}
+        onChange={handleFilterChange}
+      >
         <option>Sort By</option>
         <option value=''>Price (low to high)</option>
         <option value=''>Price (high to low)</option>
