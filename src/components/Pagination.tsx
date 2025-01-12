839425726d0546615b64bacc39a36e71a809f9ad
Noi-Git <noi.patsin@gmail.com>
2025-01-12 17:04:03 -0500

    add disabled property to the button based on offset

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,6 +1,9 @@
 'use client'
 
-import { paginationButton } from './Styles/PaginationStyle'
+import {
+  paginationNextButton,
+  paginationPrevButton,
+} from './Styles/PaginationStyle'
 
 const Pagination = ({
   currentPage,
@@ -11,10 +14,15 @@ const Pagination = ({
   hasPrev: boolean
   hasNext: boolean
 }) => {
+  const disableButton = 'disabled={!hasNext}'
   return (
     <div className='mt-12 flex justify-between w-full'>
-      <button className={paginationButton}>Previous</button>
-      <button className={paginationButton}>Next</button>
+      <button className={paginationPrevButton} disabled={!hasPrev}>
+        Previous
+      </button>
+      <button className={paginationNextButton} disabled={!hasNext}>
+        Next
+      </button>
     </div>
   )
 }
--- src/components/Styles/PaginationStyle.ts
+++ src/components/Styles/PaginationStyle.ts
@@ -1,2 +1,4 @@
-export const paginationButton =
+export const paginationPrevButton =
+  'rounded-md bg-salmon-color text-white p-2 text-sm w-24 cursor-pointer disabled:cursor-not-allowed disabled:bg-pink-200'
+export const paginationNextButton =
   'rounded-md bg-salmon-color text-white p-2 text-sm w-24 cursor-pointer disabled:cursor-not-allowed disabled:bg-pink-200'
