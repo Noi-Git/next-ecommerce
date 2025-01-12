ae3aa4a5fdaf097e4361c514a924f57d617c3220
Noi-Git <noi.patsin@gmail.com>
2025-01-12 17:11:21 -0500

    setup update url

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import {
   paginationNextButton,
   paginationPrevButton,
@@ -14,7 +15,16 @@ const Pagination = ({
   hasPrev: boolean
   hasNext: boolean
 }) => {
-  const disableButton = 'disabled={!hasNext}'
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const { replace } = useRouter()
+
+  const createPageUrl = (pageNumber: number) => {
+    const params = new URLSearchParams(searchParams)
+    params.set('page', pageNumber.toString())
+    replace(`${pathname}?${params.toString()}`)
+  }
+
   return (
     <div className='mt-12 flex justify-between w-full'>
       <button className={paginationPrevButton} disabled={!hasPrev}>
