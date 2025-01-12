d04b08213a59f03169c4c51d859d7b6830824d4b
Noi-Git <noi.patsin@gmail.com>
2025-01-12 13:47:36 -0500

    add sort by value

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -65,8 +65,8 @@ const Filter = () => {
         onChange={handleFilterChange}
       >
         <option>Sort By</option>
-        <option value='asc'>Price (low to high)</option>
-        <option value='desc'>Price (high to low)</option>
+        <option value='asc price'>Price (low to high)</option>
+        <option value='desc price'>Price (high to low)</option>
         <option value='asc lastUpdated'>Newest</option>
         <option value='desc lastUpdated'>Oldest</option>
       </select>
